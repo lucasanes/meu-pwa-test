f038313ed08ebc8bbd720b2043686af7b679e678
Lucas (Lightz) <sky.lucas003@gmail.com>
2025-08-17 11:13:30 -0300

    feat: add PWA support with manifest and service worker
    - Created a new manifest.json file for the Progressive Web App (PWA) configuration, including theme color, icons, and app details.
    - Added a service worker (sw.js) to handle caching strategies for various asset types, improving offline capabilities and performance.
    - Implemented a custom service worker (swe-worker-5c72df51bb1f6ee0.js) to manage frontend navigation caching and aggressive caching strategies.
    - Integrated Workbox library (workbox-f1770938.js) for enhanced caching and routing functionalities.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import nextPwa from '@ducanh2912/next-pwa';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+const withPWA = nextPwa({
+  dest: 'public',
+  register: true,
+  disable: process.env.NODE_ENV === 'development',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+})(nextConfig);
+
+export default withPWA;
